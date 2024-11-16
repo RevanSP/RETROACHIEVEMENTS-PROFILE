680e728f3e1b4d02fa54b062f06df55b5108a696
RevanSP <revanspstudy28@gmail.com>
2024-11-16 15:53:43 +0700

    add ngrok for test local online

--- src/hooks/useUserProfile.ts
+++ src/hooks/useUserProfile.ts
@@ -1,5 +1,5 @@
-import { useState, useEffect, useCallback } from 'react';
-import { UseUserProfileResponse, UserProfileData, UserCompletedGame, UserAward, AwardCounts, GameInfo, CachedUserProfileData, ConsoleData } from '../types/type';
+import { useState, useEffect } from 'react';
+import { UseUserProfileResponse, UserProfileData, UserCompletedGame, UserAward, AwardCounts, GameInfo, ConsoleData } from '../types/type';
 
 const useUserProfile = (username: string): UseUserProfileResponse => {
   const [userData, setUserData] = useState<UserProfileData | null>(null);
@@ -11,46 +11,17 @@ const useUserProfile = (username: string): UseUserProfileResponse => {
   const [awardCounts, setAwardCounts] = useState<AwardCounts | null>(null);
   const [consoleData, setConsoleData] = useState<ConsoleData[]>([]);
 
-  const isValidUsername = (username: string): boolean => {
-    return /^[a-zA-Z0-9]+$/.test(username);
-  };
+  const isValidUsername = (username: string): boolean => /^[a-zA-Z0-9]+$/.test(username);
 
-  const saveToCache = (username: string, data: CachedUserProfileData) => {
-    const cacheData = {
-      timestamp: new Date().getTime(),
-      data: data,
-    };
-    localStorage.setItem(`userProfile_${username}`, JSON.stringify(cacheData));
-  };
-
-  const loadFromCache = useCallback((username: string) => {
-    const CACHE_EXPIRY_TIME = 1000 * 60 * 5;
-
-    const cachedData = localStorage.getItem(`userProfile_${username}`);
-    if (cachedData) {
-      const parsedData = JSON.parse(cachedData);
-      const timestamp = parsedData.timestamp;
-      const currentTime = new Date().getTime();
-
-      if (currentTime - timestamp < CACHE_EXPIRY_TIME) {
-        return parsedData.data;
-      }
-    }
-    return null;
-  }, []);
-
-  // Fetch console data
   useEffect(() => {
     const fetchConsoleData = async () => {
       const consoleApiKey = import.meta.env.VITE_API_KEY_PROFILE;
-      const consoleUrl = `https://retroachievements.org/API/API_GetConsoleIDs.php?z=${username}&y=${consoleApiKey}&g=1`; // Fetch only game systems
+      const consoleUrl = `https://retroachievements.org/API/API_GetConsoleIDs.php?z=${username}&y=${consoleApiKey}&g=1`;
 
       try {
         const response = await fetch(consoleUrl);
         const data = await response.json();
-        if (data) {
-          setConsoleData(data); // Save the console data
-        }
+        setConsoleData(data);
       } catch (err) {
         console.error('Error fetching console data:', err);
       }
@@ -64,81 +35,36 @@ const useUserProfile = (username: string): UseUserProfileResponse => {
       setUserData(null);
       setCompletedGames(null);
       setUserAwards(null);
-      setGameInfo(null);
       setAwardCounts(null);
-      setLoading(false);
-      return;
-    }
-
-    const cachedData = loadFromCache(username);
-    if (cachedData) {
-      setUserData(cachedData.userData);
-      setCompletedGames(cachedData.completedGames);
-      setUserAwards(cachedData.userAwards);
-      setAwardCounts(cachedData.awardCounts);
       setLoading(false);
       return;
     }
 
     const fetchUserData = async () => {
       setLoading(true);
-    
+
       const profileApiKey = import.meta.env.VITE_API_KEY_PROFILE;
       const profileUrl = `https://retroachievements.org/API/API_GetUserProfile.php?u=${username}&y=${profileApiKey}`;
       const awardsUrl = `https://retroachievements.org/API/API_GetUserAwards.php?u=${username}&y=${profileApiKey}`;
       const completedGamesUrl = `https://retroachievements.org/API/API_GetUserCompletedGames.php?u=${username}&y=${profileApiKey}`;
-    
+
       try {
-        const profileResponse = await fetch(profileUrl);
+        const [profileResponse, awardsResponse, completedGamesResponse] = await Promise.all([
+          fetch(profileUrl), 
+          fetch(awardsUrl), 
+          fetch(completedGamesUrl)
+        ]);
+
         const profileData = await profileResponse.json();
-    
-        if (profileData.Error) {
-          setError('Error fetching user profile');
-          setUserData(null);
-          setCompletedGames(null);
-          setUserAwards(null);
-          setAwardCounts(null);
+        const awardsData = await awardsResponse.json();
+        const completedGamesData = await completedGamesResponse.json();
+
+        if (profileData.Error || awardsData.Error || completedGamesData.Error) {
+          setError('Error fetching user data');
           setLoading(false);
           return;
         }
-    
-        const awardsResponse = await fetch(awardsUrl);
-        const awardsData = await awardsResponse.json();
-    
-        if (awardsData.Error) {
-          setError('Error fetching user awards');
-          setUserData(profileData);
-          setCompletedGames(null);
-          setUserAwards(null);
-          setAwardCounts(null);
-          setLoading(false);
-          return; // Stop execution if there was an error with awards data
-        }
-    
-        // Fetch completed games data
-        const completedGamesResponse = await fetch(completedGamesUrl);
-        const completedGamesData = await completedGamesResponse.json();
-    
-        // Check for any error in completed games response
-        if (completedGamesData.Error) {
-          setError('Error fetching completed games');
-          setUserData(profileData);
-          setCompletedGames(null);
-          setUserAwards(awardsData.VisibleUserAwards);
-          setAwardCounts({
-            TotalAwardsCount: awardsData.TotalAwardsCount,
-            HiddenAwardsCount: awardsData.HiddenAwardsCount,
-            MasteryAwardsCount: awardsData.MasteryAwardsCount,
-            CompletionAwardsCount: awardsData.CompletionAwardsCount,
-            BeatenHardcoreAwardsCount: awardsData.BeatenHardcoreAwardsCount,
-            BeatenSoftcoreAwardsCount: awardsData.BeatenSoftcoreAwardsCount,
-            EventAwardsCount: awardsData.EventAwardsCount,
-            SiteAwardsCount: awardsData.SiteAwardsCount,
-          });
-          setLoading(false);
-          return; 
-        }
-    
+
         setUserData(profileData);
         setCompletedGames(completedGamesData);
         setUserAwards(awardsData.VisibleUserAwards);
@@ -153,24 +79,8 @@ const useUserProfile = (username: string): UseUserProfileResponse => {
           SiteAwardsCount: awardsData.SiteAwardsCount,
         });
         setError(null);
-    
-        saveToCache(username, {
-          userData: profileData,
-          completedGames: completedGamesData,
-          userAwards: awardsData.VisibleUserAwards,
-          awardCounts: {
-            TotalAwardsCount: awardsData.TotalAwardsCount,
-            HiddenAwardsCount: awardsData.HiddenAwardsCount,
-            MasteryAwardsCount: awardsData.MasteryAwardsCount,
-            CompletionAwardsCount: awardsData.CompletionAwardsCount,
-            BeatenHardcoreAwardsCount: awardsData.BeatenHardcoreAwardsCount,
-            BeatenSoftcoreAwardsCount: awardsData.BeatenSoftcoreAwardsCount,
-            EventAwardsCount: awardsData.EventAwardsCount,
-            SiteAwardsCount: awardsData.SiteAwardsCount,
-          },
-        });
-    
         setLoading(false);
+
       } catch (err) {
         setError('Error fetching user data');
         setLoading(false);
@@ -179,10 +89,11 @@ const useUserProfile = (username: string): UseUserProfileResponse => {
     };
 
     fetchUserData();
-  }, [username, loadFromCache]);
+  }, [username]);
 
   const fetchGameInfo = async (gameID: number) => {
     if (!gameID) return;
+
     const apiKey = import.meta.env.VITE_API_KEY_PROFILE;
     const gameInfoUrl = `https://retroachievements.org/API/API_GetGameInfoAndUserProgress.php?g=${gameID}&u=${username}&y=${apiKey}`;
 
@@ -196,18 +107,17 @@ const useUserProfile = (username: string): UseUserProfileResponse => {
     }
   };
 
-  return { 
-    userData, 
-    completedGames, 
-    userAwards, 
-    loading, 
-    error, 
-    fetchGameInfo, 
-    gameInfo, 
-    awardCounts, 
+  return {
+    userData,
+    completedGames,
+    userAwards,
+    loading,
+    error,
+    fetchGameInfo,
+    gameInfo,
+    awardCounts,
     consoleData,
   };
 };
-
 
 export default useUserProfile;
