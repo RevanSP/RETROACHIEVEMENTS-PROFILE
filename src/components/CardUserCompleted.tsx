5b090ab5fe111708d846df41c5e5019947ef198b
RevanSP <revanspstudy28@gmail.com>
2024-11-16 07:55:22 +0700

    back

--- src/components/CardUserCompleted.tsx
+++ src/components/CardUserCompleted.tsx
@@ -62,12 +62,7 @@ const CardUserCompleted: React.FC<Props> = ({
                                         <progress className="progress progress-info w-56" value={game.PctWon * 100} max="100"></progress>
                                         <div className="mt-0 text-xs">{(game.PctWon * 100).toFixed(2)}%</div>
                                     </td>
-                                    <td className="flex items-center justify-center text-center mt-3">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="w-4 bi bi-trophy-fill mr-2 text-yellow-500" viewBox="0 0 16 16">
-                                            <path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5q0 .807-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33 33 0 0 1 2.5.5m.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935m10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935" />
-                                        </svg>
-                                        {game.NumAwarded} / {game.MaxPossible}
-                                    </td>
+                                    <td>{game.NumAwarded} / {game.MaxPossible} Achievements</td>
                                     <td className="text-center">
                                         {game.HardcoreMode === "1" ? (
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-circle-fill mx-auto text-green-500" viewBox="0 0 16 16">
@@ -100,6 +95,7 @@ const CardUserCompleted: React.FC<Props> = ({
                             </tr>
                         )}
                     </table>
+
                     <div className="mt-4 flex justify-center">
                         <div className="join mt-4 overflow-x-auto">
                             {pageNumbers.map((pageNumber) => (
